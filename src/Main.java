eedadcd0af5cb783d7546db264c201e9ff17ee88
ars1992 <alessandro.salomon@gmx.de>
2023-08-21 13:55:10 +0200

    menue fertig

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,54 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
-    Scanner eingabe = new Scanner(System.in);
+    static Scanner eingabe = new Scanner(System.in);
 
     public static void main(String[] args) {
+        menueführung();
+
+    }
+
+    private static void menueanzeigen(){
         System.out.println("""
                 ---Menue---
                 1. Addieren
-                2. Subtrahiren
-                3. Multipliziren
+                2. Subtrahieren
+                3. Multiplication
                 4. Dividieren
                 5. Exit
                 """);
     }
 
+    private static void menueführung() {
+        while (true) {
+            menueanzeigen();
+            System.out.println("Bitte Menüpunkt auswählen: ");
+            int operation = eingabe.nextInt();
+            switch (operation) {
+                case 1 -> { addition();
+                }
+                case 2 -> { subtrahieren();
+                }
+                case 3 -> { multiplication();
+                }
+                case 4 -> { dividieren();
+                }
+                case 5 -> {
+                    return;
+                }
+            }
+        }
+    }
+
+    private static void dividieren() {
+    }
+
+    private static void multiplication() {
+    }
+
+    private static void subtrahieren() {
+    }
+
+    private static void addition() {
+    }
 }
