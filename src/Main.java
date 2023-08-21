0a11f5ee65980db7f4de8f023a81b871225866d6
ars1992 <alessandro.salomon@gmx.de>
2023-08-21 13:43:07 +0200

    initial und menue

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    Scanner eingabe = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("""
+                ---Menue---
+                1. Addieren
+                2. Subtrahiren
+                3. Multipliziren
+                4. Dividieren
+                5. Exit
+                """);
+    }
+
+}
